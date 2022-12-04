bb2caca38847448cef59853fb256ee8bff6450ae
mercz <mercz@hotmail.com>
2022-12-04 08:31:20 +0100

    spinner_inAsyncCall

--- lib/pages/Page3/flash_chat/screens/login_screen.dart
+++ lib/pages/Page3/flash_chat/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:x01_flutter_basics/pages/Page3/flash_chat/constants.dart';
 
 import 'chat_screen.dart';
@@ -15,7 +16,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   late AnimationController animationController;
-
+  bool showSpinner = false;
   //[auth]
   final FirebaseAuth _auth = FirebaseAuth.instance;
   String email = '';
@@ -40,134 +41,150 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: const Icon(
-                      Icons.arrow_back_rounded,
-                      size: 40,
+      //[spinner_inAsyncCall]
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back_rounded,
+                        size: 40,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
                     ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-              //[HeroAnimation]
-              Hero(
-                tag: kLogoTag,
-                key: Key('logo_login'),
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('assets/flash_chat/logo.png'),
+                  ],
                 ),
-              ),
-              const SizedBox(
-                height: 48.0,
-              ),
-              TextField(
+                //[HeroAnimation]
+                Hero(
+                  tag: kLogoTag,
+                  key: Key('logo_login'),
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('assets/flash_chat/logo.png'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                    cursorColor: Colors.white,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    //[kConst.copywith]
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your email')),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password.'),
+                  obscureText: true,
                   cursorColor: Colors.white,
                   textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  //[kConst.copywith]
-                  decoration: kTextFieldDecoration.copyWith(
-                      hintText: 'Enter your email')),
-              const SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter your password.'),
-                obscureText: true,
-                cursorColor: Colors.white,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Material(
-                  color: Colors.lightBlueAccent,
-                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  child: MaterialButton(
-                    onPressed: () async {
-                      try {
-                        //[auth]
-                        UserCredential newUser =
-                            await _auth.signInWithEmailAndPassword(
-                                email: email, password: password);
-                        if (newUser.user != null) {
-                          Navigator.pushNamed(context, ChatScreen.id);
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                    color: Colors.lightBlueAccent,
+                    borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                    elevation: 5.0,
+                    child: MaterialButton(
+                      onPressed: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          //[auth]
+                          UserCredential newUser =
+                              await _auth.signInWithEmailAndPassword(
+                                  email: email, password: password);
+                          if (newUser.user != null) {
+                            Navigator.pushNamed(context, ChatScreen.id);
+                          }
+                        } catch (e) {
+                          print(e);
                         }
-                      } catch (e) {
-                        print(e);
-                      }
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: const Text(
-                      'Log In',
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      },
+                      minWidth: 200.0,
+                      height: 42.0,
+                      child: const Text(
+                        'Log In',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Material(
-                  color: Colors.red,
-                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  //[GoogleSignIn]
-                  child: MaterialButton(
-                    onPressed: () async {
-                      try {
-                        OAuthCredential credential = await googleLogin();
-                        UserCredential newUser =
-                            await _auth.signInWithCredential(credential);
-                        if (newUser.user != null) {
-                          Navigator.pushNamed(context, ChatScreen.id);
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                    color: Colors.red,
+                    borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                    elevation: 5.0,
+                    //[GoogleSignIn]
+                    child: MaterialButton(
+                      onPressed: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          OAuthCredential credential = await googleLogin();
+                          UserCredential newUser =
+                              await _auth.signInWithCredential(credential);
+                          if (newUser.user != null) {
+                            Navigator.pushNamed(context, ChatScreen.id);
+                          }
+                        } catch (e) {
+                          print(e);
                         }
-                      } catch (e) {
-                        print(e);
-                      }
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        FaIcon(
-                          FontAwesomeIcons.google,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'Login with Google',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      },
+                      minWidth: 200.0,
+                      height: 42.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(
+                            FontAwesomeIcons.google,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Login with Google',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/Page3/flash_chat/screens/registration_screen.dart
+++ lib/pages/Page3/flash_chat/screens/registration_screen.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
 import 'package:x01_flutter_basics/pages/Page3/flash_chat/constants.dart';
 import 'package:x01_flutter_basics/pages/Page3/flash_chat/screens/chat_screen.dart';
-import 'package:x01_flutter_basics/pages/Page3/flash_chat/constants.dart';
 
 /*
 [Keyboard] [Error01]: when using textfields, the keyboard pops up and you get pixels overflow error
@@ -24,6 +23,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  bool showSpinner = false;
   //[auth]
   final FirebaseAuth _auth = FirebaseAuth.instance;
   String email = '';
@@ -49,163 +49,179 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     return Scaffold(
       //[Error01]: resizeToAvoidBottomInset avoid textinput to disappear (resizes?) when keyboard appears
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        //[Error01]: SingleChildScrollView avoid pixels overflow
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: const Icon(
-                      Icons.arrow_back_rounded,
-                      size: 40,
+      //[spinner_inAsyncCall]
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          //[Error01]: SingleChildScrollView avoid pixels overflow
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back_rounded,
+                        size: 40,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
                     ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-              //[HeroAnimation]
-              Hero(
-                tag: kLogoTag,
-                key: const Key('logo_registration'),
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('assets/flash_chat/logo.png'),
+                  ],
                 ),
-              ),
-              const SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                cursorColor: Colors.white,
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration:
-                    kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                cursorColor: Colors.white,
-                textAlign: TextAlign.center,
-                obscureText: passwordObscureText,
-                //keyboardType: TextInputType.visiblePassword,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your password',
-                  suffixIcon: GestureDetector(
-                    child: Icon(Icons.visibility,
-                        color: passwordObscureText == true
-                            ? Colors.white
-                            : Colors.grey),
-                    onTap: () {
-                      setState(() {
-                        passwordObscureText =
-                            passwordObscureText == true ? false : true;
-                      });
-                    },
+                //[HeroAnimation]
+                Hero(
+                  tag: kLogoTag,
+                  key: const Key('logo_registration'),
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('assets/flash_chat/logo.png'),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Material(
-                  color: Colors.blueAccent,
-                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  child: MaterialButton(
-                    onPressed: () async {
-                      try {
-                        //[auth] password hast to be at least 6 characters long
-                        UserCredential newUser =
-                            await _auth.createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        if (newUser.user != null) {
-                          Navigator.pushNamed(context, ChatScreen.id);
+                const SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                  cursorColor: Colors.white,
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your email'),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  cursorColor: Colors.white,
+                  textAlign: TextAlign.center,
+                  obscureText: passwordObscureText,
+                  //keyboardType: TextInputType.visiblePassword,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your password',
+                    suffixIcon: GestureDetector(
+                      child: Icon(Icons.visibility,
+                          color: passwordObscureText == true
+                              ? Colors.white
+                              : Colors.grey),
+                      onTap: () {
+                        setState(() {
+                          passwordObscureText =
+                              passwordObscureText == true ? false : true;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                    color: Colors.blueAccent,
+                    borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                    elevation: 5.0,
+                    child: MaterialButton(
+                      onPressed: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          //[auth] password hast to be at least 6 characters long
+                          UserCredential newUser =
+                              await _auth.createUserWithEmailAndPassword(
+                                  email: email, password: password);
+                          if (newUser.user != null) {
+                            Navigator.pushNamed(context, ChatScreen.id);
+                          }
+                        } catch (e) {
+                          print(e);
                         }
-                      } catch (e) {
-                        print(e);
-                      }
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.email,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'Register with Email',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      },
+                      minWidth: 200.0,
+                      height: 42.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.email,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Register with Email',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Material(
-                  color: Colors.red,
-                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  //[GoogleSignIn]
-                  child: MaterialButton(
-                    onPressed: () async {
-                      try {
-                        OAuthCredential credential = await googleLogin();
-                        UserCredential newUser =
-                            await _auth.signInWithCredential(credential);
-                        if (newUser.user != null) {
-                          Navigator.pushNamed(context, ChatScreen.id);
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                    color: Colors.red,
+                    borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                    elevation: 5.0,
+                    //[GoogleSignIn]
+                    child: MaterialButton(
+                      onPressed: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          OAuthCredential credential = await googleLogin();
+                          UserCredential newUser =
+                              await _auth.signInWithCredential(credential);
+                          if (newUser.user != null) {
+                            Navigator.pushNamed(context, ChatScreen.id);
+                          }
+                        } catch (e) {
+                          print(e);
                         }
-                      } catch (e) {
-                        print(e);
-                      }
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        FaIcon(
-                          FontAwesomeIcons.google,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'Register with Google',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      },
+                      minWidth: 200.0,
+                      height: 42.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(
+                            FontAwesomeIcons.google,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Register with Google',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
