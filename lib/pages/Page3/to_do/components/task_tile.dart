37277788a9e31fbdaf62b1bf90140264133d0271
mercz <mercz@hotmail.com>
2023-01-25 17:54:52 +0100

    CallBack use

--- lib/pages/Page3/to_do/components/task_tile.dart
+++ lib/pages/Page3/to_do/components/task_tile.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
       textColor: Colors.black87,
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        side: const BorderSide(
-          color: Colors.black87,
-          width: 1.5,
-        ),
-        value: false,
-        onChanged: ((chaged) {}),
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        //[CallBack Function] to setState down the tree
+        toggleCheckBoxState: (bool? newValue) {
+          setState(() {
+            isChecked = newValue!;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function(bool?) toggleCheckBoxState; //[CallBack Function]
+
+  TaskCheckBox(
+      {required this.checkBoxState, required this.toggleCheckBoxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      side: const BorderSide(
+        color: Colors.black87,
+        width: 1.5,
+      ),
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState,
+      activeColor: Colors.lightBlueAccent,
     );
   }
 }
